6eaa816b963ed22661d6257a1bfa1ecda1ff8ecb
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-09 17:17:31 +0300

    task_13

--- basic_func13.py
+++ basic_func13.py
@@ -1,3 +1,12 @@
 # Create a function called main.
 # Create function argument a.
 # Returns the square root of a number.
+
+from math import sqrt
+
+
+def main(a):
+    return sqrt(a)
+
+a = pow(6, 2)
+print(main(a))
