0c6e6f05a5cc665ecde9fb9de568aecfeb0aced1
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-09 18:06:09 +0300

    task_14

--- basic_func14.py
+++ basic_func14.py
@@ -1,3 +1,12 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Returns the floor division of a and b.
+from math import floor
+
+def main(a, b):
+    value = floor(a), floor(b)
+    return value
+
+a = 23.8736
+b = 26.999
+print(main(a, b))
