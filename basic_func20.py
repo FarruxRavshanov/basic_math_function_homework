f6abceb0a8f7692b932ca3a515148e1d664992da
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-14 20:26:08 +0300

    task20

--- basic_func20.py
+++ basic_func20.py
@@ -1,9 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the multiplication of a and b.
-def main():
+def main(a, b):
     a = 5
     b = 4
     return a * b
 
-print(main())
+print(main(5, 4))
