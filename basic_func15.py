387ebde4cd53bf1bcd2b7e776dffa3a8734681d7
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-14 15:20:31 +0300

    task15

--- basic_func15.py
+++ basic_func15.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the modulus of a and b.
+from math import sqrt
+
+def main(a, b):
+    return abs(a), abs(b)
+
+print(main(3.7, -6.4))
